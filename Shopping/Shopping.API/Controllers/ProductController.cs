24d887a203bb9dd4c09cc685d76ec7a9dc9e70f9
Sandeep Jain (Infosys Ltd) <v-sandej@microsoft.com>
2021-07-26 19:23:02 +0530

    Push code to main branch

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shopping.API.Models;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Data;
+using MongoDB.Driver;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ProductContext _context;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ProductContext context, ILogger<ProductController> logger)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context
+                            .Products
+                            .Find(p => true)
+                            .ToListAsync();
+        }
+    }
+}
